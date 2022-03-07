e562a076768788c86383af96fb0eb46815a77b72
onahte <ethan.oh@gmail.com>
2022-03-07 08:44:01 -0500

    Updated simple_pages_test.py

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/index">Home</a>' in response.data
+    assert b'<a class="nav-link" href="/index">Ethan's IS601 Project 1</a>' in response.data
     assert b'<a class="nav-link" href="/git">Git</a>' in response.data
     assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/python">Python+Flask</a>' in response.data
